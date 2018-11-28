569b6d2546472d3e91d5ec136ce4449c27a19457
adeliyay <adeliya.yuliani@gmail.com>
2018-11-29 06:22:36 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,41 @@
 */
 
 Route::get('/', function () {
-    return view('dashboard');
-});
-
-Route::get('/penjualan.php', function () {
-    return view('penjualan');
-});
-
-Route::get('/input.php', function () {
-    return view('input');
-});
-
-Route::get('/barang.php', function () {
-    return view('Barang');
-});
-
-Route::get('/piutang.php', function () {
-    return view('piutang');
-});
-
-Route::get('/operasional.php', function () {
-    return view('operasional');
+    return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
+Route::get('/penjualan', 'HomeController@index')->name('penjualan');
+Route::get('/barang', 'HomeController@index')->name('barang');
+Route::get('/piutang', 'HomeController@index')->name('piutang');
+Route::get('/operasional', 'HomeController@index')->name('operasional');
+
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
+
+// Route::get('/penjualan.php', function () {
+//     return view('penjualan');
+// });
+
+// Route::get('/input.php', function () {
+//     return view('input');
+// });
+
+// Route::get('/barang.php', function () {
+//     return view('Barang');
+// });
+
+// Route::get('/piutang.php', function () {
+//     return view('piutang');
+// });
+
+// Route::get('/operasional.php', function () {
+//     return view('operasional');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
